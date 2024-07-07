d3cf9481eb30b7dcfd12317b032cc778c54d2026
slashbade <slashbladevitali@gmail.com>
2024-07-07 18:23:59 +0800

    t

---
+++ .lake/build/ir/PekingNote/Basic.c
@@ -0,0 +1,280 @@
+// Lean compiler output
+// Module: PekingNote.Basic
+// Imports: Init Mathlib.Algebra.Group.Centralizer Mathlib.GroupTheory.GroupAction.Basic Mathlib.GroupTheory.Perm.Basic Mathlib.GroupTheory.GroupAction.ConjAct Mathlib.GroupTheory.GroupAction.Quotient Mathlib.Deprecated.Subgroup Mathlib.GroupTheory.QuotientGroup
+#include <lean/lean.h>
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-label"
+#elif defined(__GNUC__) && !defined(__CLANG__)
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-label"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer(lean_object*);
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__2(lean_object*);
+LEAN_EXPORT lean_object* l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3___rarg(lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg___lambda__1___boxed(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__2___rarg(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg___lambda__2(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__1___rarg___boxed(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_instMulActionPerm__pekingNote(lean_object*);
+lean_object* l_Monoid_toMulAction___rarg___boxed(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__2___rarg___boxed(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg___lambda__1(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__1(lean_object*);
+LEAN_EXPORT lean_object* l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3(lean_object*);
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__1___rarg(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_MulAction_kernel(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_instMulAction__pekingNote___rarg(lean_object*);
+static lean_object* l_instMulActionPerm__pekingNote___closed__1;
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg___lambda__2___boxed(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_instMulAction__pekingNote(lean_object*);
+lean_object* l_Equiv_Perm_applyMulAction___rarg(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3___rarg___boxed(lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg(lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* l_MulAction_kernel___rarg(lean_object*, lean_object*);
+static lean_object* _init_l_instMulActionPerm__pekingNote___closed__1() {
+_start:
+{
+lean_object* x_1; 
+x_1 = lean_alloc_closure((void*)(l_Equiv_Perm_applyMulAction___rarg), 2, 0);
+return x_1;
+}
+}
+LEAN_EXPORT lean_object* l_instMulActionPerm__pekingNote(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = l_instMulActionPerm__pekingNote___closed__1;
+return x_2;
+}
+}
+LEAN_EXPORT lean_object* l_instMulAction__pekingNote___rarg(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; lean_object* x_3; 
+x_2 = lean_ctor_get(x_1, 0);
+lean_inc(x_2);
+lean_dec(x_1);
+x_3 = lean_alloc_closure((void*)(l_Monoid_toMulAction___rarg___boxed), 3, 1);
+lean_closure_set(x_3, 0, x_2);
+return x_3;
+}
+}
+LEAN_EXPORT lean_object* l_instMulAction__pekingNote(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = lean_alloc_closure((void*)(l_instMulAction__pekingNote___rarg), 1, 0);
+return x_2;
+}
+}
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__1___rarg(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6) {
+_start:
+{
+lean_object* x_7; 
+x_7 = lean_alloc_ctor(0, 1, 0);
+lean_ctor_set(x_7, 0, lean_box(0));
+return x_7;
+}
+}
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__1(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = lean_alloc_closure((void*)(l_iInf___at_Stabilizer_setStabilizer___spec__1___rarg___boxed), 6, 0);
+return x_2;
+}
+}
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__2___rarg(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+lean_object* x_3; 
+x_3 = lean_alloc_ctor(0, 1, 0);
+lean_ctor_set(x_3, 0, lean_box(0));
+return x_3;
+}
+}
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__2(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = lean_alloc_closure((void*)(l_iInf___at_Stabilizer_setStabilizer___spec__2___rarg___boxed), 2, 0);
+return x_2;
+}
+}
+LEAN_EXPORT lean_object* l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3___rarg(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4) {
+_start:
+{
+lean_object* x_5; 
+x_5 = lean_alloc_ctor(0, 1, 0);
+lean_ctor_set(x_5, 0, lean_box(0));
+return x_5;
+}
+}
+LEAN_EXPORT lean_object* l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = lean_alloc_closure((void*)(l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3___rarg___boxed), 4, 0);
+return x_2;
+}
+}
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg___lambda__1(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+lean_inc(x_1);
+return x_1;
+}
+}
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg___lambda__2(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; lean_object* x_5; 
+lean_inc(x_3);
+x_4 = lean_alloc_closure((void*)(l_Stabilizer_setStabilizer___rarg___lambda__1___boxed), 2, 1);
+lean_closure_set(x_4, 0, x_3);
+x_5 = l_iInf___at_Stabilizer_setStabilizer___spec__1___rarg(x_1, lean_box(0), x_2, lean_box(0), x_3, x_4);
+lean_dec(x_4);
+lean_dec(x_3);
+return x_5;
+}
+}
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4) {
+_start:
+{
+lean_object* x_5; lean_object* x_6; lean_object* x_7; 
+lean_inc(x_1);
+x_5 = lean_alloc_closure((void*)(l_Stabilizer_setStabilizer___rarg___lambda__2___boxed), 3, 2);
+lean_closure_set(x_5, 0, x_1);
+lean_closure_set(x_5, 1, x_3);
+x_6 = l_iInf___at_Stabilizer_setStabilizer___spec__2___rarg(x_1, x_5);
+lean_dec(x_5);
+x_7 = l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3___rarg(x_1, x_6, lean_box(0), lean_box(0));
+lean_dec(x_6);
+lean_dec(x_1);
+return x_7;
+}
+}
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = lean_alloc_closure((void*)(l_Stabilizer_setStabilizer___rarg), 4, 0);
+return x_2;
+}
+}
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__1___rarg___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6) {
+_start:
+{
+lean_object* x_7; 
+x_7 = l_iInf___at_Stabilizer_setStabilizer___spec__1___rarg(x_1, x_2, x_3, x_4, x_5, x_6);
+lean_dec(x_6);
+lean_dec(x_5);
+lean_dec(x_3);
+lean_dec(x_1);
+return x_7;
+}
+}
+LEAN_EXPORT lean_object* l_iInf___at_Stabilizer_setStabilizer___spec__2___rarg___boxed(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+lean_object* x_3; 
+x_3 = l_iInf___at_Stabilizer_setStabilizer___spec__2___rarg(x_1, x_2);
+lean_dec(x_2);
+lean_dec(x_1);
+return x_3;
+}
+}
+LEAN_EXPORT lean_object* l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3___rarg___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4) {
+_start:
+{
+lean_object* x_5; 
+x_5 = l_Submonoid_copy___at_Stabilizer_setStabilizer___spec__3___rarg(x_1, x_2, x_3, x_4);
+lean_dec(x_2);
+lean_dec(x_1);
+return x_5;
+}
+}
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg___lambda__1___boxed(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+lean_object* x_3; 
+x_3 = l_Stabilizer_setStabilizer___rarg___lambda__1(x_1, x_2);
+lean_dec(x_1);
+return x_3;
+}
+}
+LEAN_EXPORT lean_object* l_Stabilizer_setStabilizer___rarg___lambda__2___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; 
+x_4 = l_Stabilizer_setStabilizer___rarg___lambda__2(x_1, x_2, x_3);
+lean_dec(x_2);
+lean_dec(x_1);
+return x_4;
+}
+}
+LEAN_EXPORT lean_object* l_MulAction_kernel___rarg(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+lean_object* x_3; 
+x_3 = l_Stabilizer_setStabilizer___rarg(x_1, lean_box(0), x_2, lean_box(0));
+return x_3;
+}
+}
+LEAN_EXPORT lean_object* l_MulAction_kernel(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+lean_object* x_3; 
+x_3 = lean_alloc_closure((void*)(l_MulAction_kernel___rarg), 2, 0);
+return x_3;
+}
+}
+lean_object* initialize_Init(uint8_t builtin, lean_object*);
+lean_object* initialize_Mathlib_Algebra_Group_Centralizer(uint8_t builtin, lean_object*);
+lean_object* initialize_Mathlib_GroupTheory_GroupAction_Basic(uint8_t builtin, lean_object*);
+lean_object* initialize_Mathlib_GroupTheory_Perm_Basic(uint8_t builtin, lean_object*);
+lean_object* initialize_Mathlib_GroupTheory_GroupAction_ConjAct(uint8_t builtin, lean_object*);
+lean_object* initialize_Mathlib_GroupTheory_GroupAction_Quotient(uint8_t builtin, lean_object*);
+lean_object* initialize_Mathlib_Deprecated_Subgroup(uint8_t builtin, lean_object*);
+lean_object* initialize_Mathlib_GroupTheory_QuotientGroup(uint8_t builtin, lean_object*);
+static bool _G_initialized = false;
+LEAN_EXPORT lean_object* initialize_PekingNote_Basic(uint8_t builtin, lean_object* w) {
+lean_object * res;
+if (_G_initialized) return lean_io_result_mk_ok(lean_box(0));
+_G_initialized = true;
+res = initialize_Init(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Mathlib_Algebra_Group_Centralizer(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Mathlib_GroupTheory_GroupAction_Basic(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Mathlib_GroupTheory_Perm_Basic(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Mathlib_GroupTheory_GroupAction_ConjAct(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Mathlib_GroupTheory_GroupAction_Quotient(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Mathlib_Deprecated_Subgroup(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Mathlib_GroupTheory_QuotientGroup(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+l_instMulActionPerm__pekingNote___closed__1 = _init_l_instMulActionPerm__pekingNote___closed__1();
+lean_mark_persistent(l_instMulActionPerm__pekingNote___closed__1);
+return lean_io_result_mk_ok(lean_box(0));
+}
+#ifdef __cplusplus
+}
+#endif
